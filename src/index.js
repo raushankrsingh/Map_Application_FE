afd6dfe74b8fe447e609ae62d05146a23345b443
Raushan Kumar <singhraushan9923@gmail.com>
2024-04-17 19:20:57 +0530

    added backend url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from "axios"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+axios.defaults.baseURL = 'https://map-application-backend.onrender.com/api'
 root.render(
   <React.StrictMode>
     <App />
